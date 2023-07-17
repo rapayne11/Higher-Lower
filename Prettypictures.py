48cfee3f182fcd1d025bba219932cec3cfdcf84a
Ryan Payne <rapayne03@gmail.com>
2023-07-17 10:37:37 -0700

    Pretty pictures no nose

---
+++ Prettypictures.py
@@ -0,0 +1,9 @@
+from gasp import *
+... begin_graphics()
+... Circle ((300, 200), 100)
+... Circle ((340,230), 20)
+... Circle ((270, 230), 20)
+... Arc((300, 200), 30, 225, 315)
+... 
+... update_when('key pressed')
+end_graphics()
