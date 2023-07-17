ad648ec60798957d703461bd489dbc40842dc788
Ryan Payne <rapayne03@gmail.com>
2023-07-17 09:50:41 -0700

    trial 1 robots

---
+++ The Robots are coming.py
@@ -0,0 +1,31 @@
+from gasp import *
+begin_graphics()
+finished = False
+
+def place_player ():
+    print ("Here I am!")
+
+player_x= rat(0,57) *10
+player_y= rat(0,40) *10
+circle ((10 * player_x +5, 10 * player_y + 5), 5, filled = True)
+
+def move_player():
+    print("I'm moving...")
+    update_when ('key_pressed') 
+
+place_player ()
+
+
+While not finished:
+    moving_player()
+
+while True:
+    x= +4
+    y= +3
+    circle ((x,y), 5)
+    ball_x=cirlce((x,y) 5)
+    move_to
+if x> 635=break()
+
+end_graphics ()
+
